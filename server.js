a618ef4a3d77b6c4b6af6c933c209908cedda10c
3r3n-n <82424931+3r3n-n@users.noreply.github.com>
2021-05-09 17:55:47 -0500

    added require(mongoose) to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require('./app'); // mmm...
+const mongoose = require(‘mongoose’);
 
 const irb = `mongodb+srv://${process.env.CONTACTS_DB_USER}:${process.env.CONTACTS_DB_PASSWORD}@mycontactsappasies.mbffy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // Connection to DB
